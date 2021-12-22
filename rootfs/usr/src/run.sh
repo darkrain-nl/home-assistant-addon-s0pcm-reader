bfff0561882b507674b28d3bc8dc23e3f3109a39
darkrain-nl <jan@darkrain.nl>
2021-12-22 16:57:41 +0100

    make it possible to use the 5 meter version as well

--- rootfs/usr/src/run.sh
+++ rootfs/usr/src/run.sh
@@ -6,6 +6,9 @@ MEASUREMENT="/data/config/measurement.yaml"
 # Initialize measurement.yaml
 meter1=$(bashio::config 'meter_1')
 meter2=$(bashio::config 'meter_2')
+meter3=$(bashio::config 'meter_3')
+meter4=$(bashio::config 'meter_4')
+meter5=$(bashio::config 'meter_5')
 
 {
     echo date: $(date '+%Y-%m-%d')
@@ -18,6 +21,21 @@ meter2=$(bashio::config 'meter_2')
     echo "  pulsecount: 0"
     echo "  today: 0"
     echo "  total: ${meter2}"
+    echo "  yesterday: 0"
+    echo "3:"
+    echo "  pulsecount: 0"
+    echo "  today: 0"
+    echo "  total: ${meter3}"
+    echo "  yesterday: 0"
+    echo "4:"
+    echo "  pulsecount: 0"
+    echo "  today: 0"
+    echo "  total: ${meter4}"
+    echo "  yesterday: 0"
+    echo "5:"
+    echo "  pulsecount: 0"
+    echo "  today: 0"
+    echo "  total: ${meter5}"
     echo "  yesterday: 0"
 } > "${MEASUREMENT}"
 
