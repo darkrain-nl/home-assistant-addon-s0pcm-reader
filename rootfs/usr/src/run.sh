94ca25a01a8038e01a9b916ab3d37d2542a5b66b
darkrain-nl <jan@darkrain.nl>
2024-02-13 17:17:04 +0100

    Add version to log

--- rootfs/usr/src/run.sh
+++ rootfs/usr/src/run.sh
@@ -6,6 +6,12 @@ if [ ! -f /share/s0pcm/configuration.json ]; then
   cp /usr/src/configuration.json.example /share/s0pcm/configuration.json
 fi
 
+
+VERSION=$(bashio::addon.version)
+echo "ADDON VERSION: ${VERSION}"
+UPDATEAVAIL=$(bashio::addon.update_available)
+echo "ADDON UPDATE AVAILABLE: ${UPDATEAVAIL}"
+
 python /usr/src/s0pcm-reader.py -c /share/s0pcm
 
 if [ -f /share/s0pcm/.noexit ]; then
