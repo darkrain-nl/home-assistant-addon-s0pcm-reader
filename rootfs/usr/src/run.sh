bac8a4f44f11d45a1c927fff4d9097a2ec57b121
darkrain-nl <jan@darkrain.nl>
2024-02-13 14:45:22 +0100

    Add version to log

--- rootfs/usr/src/run.sh
+++ rootfs/usr/src/run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bashio
 set -e
+declare s0pcm_version
 
 # Copy example configuration if not exists
 if [ ! -f /share/s0pcm/configuration.json ]; then
   cp /usr/src/configuration.json.example /share/s0pcm/configuration.json
 fi
+
+s0pcm_version = bashio::addon.version()
+bashio::log.info "Starting S0PCm Reader... version ${s0pcm_version}"
 
 python /usr/src/s0pcm-reader.py -c /share/s0pcm
 
