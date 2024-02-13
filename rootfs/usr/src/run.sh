22490aeabd5740abbd89c9d5eec6647fd39c92c6
darkrain-nl <jan@darkrain.nl>
2024-02-13 17:56:38 +0100

    Add version to log

--- rootfs/usr/src/run.sh
+++ rootfs/usr/src/run.sh
@@ -1,17 +1,44 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
+
+if bashio::supervisor.ping; then
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+    bashio::log.blue " Add-on: $(bashio::addon.name)"
+    bashio::log.blue " $(bashio::addon.description)"
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+
+    bashio::log.blue " Add-on version: $(bashio::addon.version)"
+    if bashio::var.true "$(bashio::addon.update_available)"; then
+        bashio::log.magenta ' There is an update available for this add-on!'
+        bashio::log.magenta \
+            " Latest add-on version: $(bashio::addon.version_latest)"
+        bashio::log.magenta ' Please consider upgrading as soon as possible.'
+    else
+        bashio::log.green ' You are running the latest version of this add-on.'
+    fi
+
+    bashio::log.blue " System: $(bashio::info.operating_system)" \
+        " ($(bashio::info.arch) / $(bashio::info.machine))"
+    bashio::log.blue " Home Assistant Core: $(bashio::info.homeassistant)"
+    bashio::log.blue " Home Assistant Supervisor: $(bashio::info.supervisor)"
+
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+    bashio::log.blue \
+        ' Please, share the above information when looking for help'
+    bashio::log.blue \
+        ' or support in, e.g., GitHub, forums or the Discord chat.'
+    bashio::log.blue \
+        '-----------------------------------------------------------'
+fi
+
 set -e
 
 # Copy example configuration if not exists
 if [ ! -f /share/s0pcm/configuration.json ]; then
   cp /usr/src/configuration.json.example /share/s0pcm/configuration.json
 fi
-
-echo "${SUPERVISOR_TOKEN}"
-
-VERSION=$(bashio::addon.version)
-echo "ADDON VERSION: ${VERSION}"
-UPDATEAVAIL=$(bashio::addon.update_available)
-echo "ADDON UPDATE AVAILABLE: ${UPDATEAVAIL}"
 
 python /usr/src/s0pcm-reader.py -c /share/s0pcm
 
